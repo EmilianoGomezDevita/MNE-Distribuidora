2478961f4bddfc656645ba2aec8c28a7759193f7
Emi-gd <egomezdevita@gmail.com>
2026-08-15 18:34:18 -0300

    funcion createClient creada y configurada

---
+++ Backend/src/config/supabase.js
@@ -0,0 +1,9 @@
+//Llamamos a la libreria dotenv y sus condiguraciones con .config()
+import dotenv from 'dotenv';
+dotenv.config();
+//llamamos a la funcion crear cliente de supabase 
+import { createClient } from '@supabase/supabase-js'
+//hacemos una const que contenga la funcion con los parametros nuestro
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY)
+//exportamos la funcion
+export {supabase}
