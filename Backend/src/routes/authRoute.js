b0b760ce9d7f85a26580b9e552ae2c64a3d4ddf8
Emi-gd <egomezdevita@gmail.com>
2026-08-19 10:51:45 -0300

    middleware multer agregado al proyecto para manejar el formato multipart/form-data, y agregado al route para que lea al archivo credencial

--- Backend/src/routes/authRoute.js
+++ Backend/src/routes/authRoute.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { registrar } from "../controllers/authController.js";
+import { upload } from "../middlewares/upload.js";
 
 const router = Router();
 
-router.post('/Registro', registrar);
+router.post('/Registro',upload.single('credencial'), registrar);
 
 export default router;
