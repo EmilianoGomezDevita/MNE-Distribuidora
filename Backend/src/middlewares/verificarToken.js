259c938c0adbac3fb12fa9c7ebabda3f6af68258
EmilianoGomezDevita <gomezdevitae@gmail.com>
2026-08-20 11:42:18 -0300

    funcion verificaionToken terminada

--- Backend/src/middlewares/verificarToken.js
+++ Backend/src/middlewares/verificarToken.js
@@ -1,6 +1,26 @@
 import { supabase } from "../config/supabase.js";
 
-async function verificaionToken(req, res) {
+async function verificacionToken(req, res, next) {
     const authHeader = req.headers.authorization;
 
+    if (!authHeader) {
+        return res.status(401).json({ 
+            mensaje: "Acceso no autorizado", 
+            error: "No se proporcionó el encabezado de autorización" 
+        });
+    }
+    const partes = authHeader.split(' ');
+    const token = partes[1]; 
+    const { data: {user}, error: errorUser } = await supabase.auth.getUser(token)
+
+    if (errorUser || !user) {
+        return res.status(401).json({ 
+            mensaje: "Token inválido o expirado", 
+            error: errorUser ? errorUser.message : "Usuario no encontrado" 
+        });
+    }
+    req.usuario = user
+    next()
 }
+
+export { verificacionToken }
