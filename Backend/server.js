0958c00b62fdfd5a47a025bfa5c70592e7559df8
EmilianoGomezDevita <gomezdevitae@gmail.com>
2026-08-21 09:41:12 -0300

    ruat de admin importada y app.use de la ruta codeado

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors'
 import dotenv from 'dotenv';
 import authRoutes from './src/routes/authRoute.js';
+import adminRoutes from './src/routes/adminRoute.js'
 import productosRoutes from './src/routes/prodRoute.js';
 
 
@@ -20,6 +21,8 @@ app.get('/', (req, res) => {
 app.use('/api/auth', authRoutes)
 
 app.use('/api/cat', productosRoutes)
+
+app.use('/api/admin', adminRoutes)
 
 app.listen(port, () => {
     console.log("Servidor corriendo en ", port)
