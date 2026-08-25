8ac10a24b6a02bf8e6d1bb27ee2655b96266f83b
EmilianoGomezDevita <gomezdevitae@gmail.com>
2026-08-25 09:55:55 -0300

    Refactor ListarProductos function to select specific fields and adjust pricing logic for professionals

--- Backend/src/controllers/listarProductos.js
+++ Backend/src/controllers/listarProductos.js
@@ -4,7 +4,18 @@ import { supabase } from "../config/supabase.js";
 async function ListarProductos(req, res) {
     const { data: dataProds, error: errorProds } = await supabase
         .from('Productos')
-        .select('*')
+        .select(`
+        id,
+        nombre,
+        descripcion,
+        precio,
+        precioProfesional,
+        stock,
+        id_marca,
+        id_categoria,
+        fechaVencimiento,
+        ImagenesProducto (url, orden)
+    `);
 
     if (errorProds) {
         return res.status(400).json({ mensaje: "Error al listar los productos", error: errorProds.message })
@@ -26,12 +37,13 @@ async function ListarProductos(req, res) {
         return {
             id: prod.id,
             nombre: prod.nombre,
-            precio: esProfesional ? Math.round(prod.precio * 0.60) : prod.precio,//esProfesional ? precioPro : prod.precio, preguntar si todos los prods tienen el mismo descuento
+            precio: esProfesional ? prod.precioProfesional : prod.precio,
             descripcion: prod.descripcion,
             stock: prod.stock,
             id_marca: prod.id_marca,
             id_categoria: prod.id_categoria,
             fechaVencimiento: prod.fechaVencimiento,
+            imagen: prod.ImagenesProducto.find(img => img.orden === 1)?.url
         }
     })
 
