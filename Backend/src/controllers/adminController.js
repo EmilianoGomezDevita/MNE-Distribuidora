cb0d39de0b3a63059fb3c38fafda574157cdd7c6
EmilianoGomezDevita <gomezdevitae@gmail.com>
2026-08-25 08:03:05 -0300

    Add actualizarSolicitud function and update admin routes

--- Backend/src/controllers/adminController.js
+++ Backend/src/controllers/adminController.js
@@ -1,25 +1,25 @@
 import { supabase } from "../config/supabase.js";
 
 async function listarSolicitudesPendientes(req, res) {
-  const { data: dataSP, error: errorSP } = await supabase
-    .from("SolicitudProfesional")
-    .select(
-      `
-    id_SP,
-    matricula,
-    documentacion,
-    estado,
-    fechaSolicitud,
-    usuarios (nombre, apellido, email),
-    Profesiones (profesion)
-  `,
-    )
-    .eq("estado", "pendiente");
+    const { data: dataSP, error: errorSP } = await supabase
+        .from("SolicitudProfesional")
+        .select(
+        `
+        id_SP,
+        matricula,
+        documentacion,
+        estado,
+        fechaSolicitud,
+        usuarios (nombre, apellido, email),
+        Profesiones (profesion)
+        `,
+        )
+        .eq("estado", "pendiente");
 
-    if(errorSP){
-        return res.status(400).json({mensaje: "Error al intentar acceder a los datos", error: errorSP.message})
+    if (errorSP) {
+        return res.status(400).json({ mensaje: "Error al intentar acceder a los datos", error: errorSP.message })
     }
-     
+
     return res.status(200).json({
         mensaje: "Informacion obtenida de manera exitosa",
         dataSP
@@ -27,4 +27,55 @@ async function listarSolicitudesPendientes(req, res) {
 
 }
 
-export {listarSolicitudesPendientes }
+async function actualizarSolicitud(req, res) {
+    const idSolicitud = req.params.id;
+    let estadoCambiado = false;
+
+    const { decision } = req.body;
+    if (decision !== 'aprobado' && decision !== 'rechazado') {
+        return res.status(400).json({ mensaje: "Decision invalida, debe ser aprobado o rechazado" })
+    }
+    try {
+        const { data: dataDecision, error: errorDesicion } = await supabase
+            .from('SolicitudProfesional')
+            .update({ estado: decision })
+            .eq('id_SP', idSolicitud)
+            .select('user_USP')// devuelve el id del usuario dueño
+            .single()
+
+        if (errorDesicion) {
+            console.log('Error completo:', errorDesicion);
+            throw new Error(errorDesicion.message);
+        }
+        estadoCambiado = true;
+        if (decision === "aprobado") {
+            const { data: dataEstado, error: errorEstado } = await supabase
+                .from('usuarios')
+                .update({ tipoCuenta: "Profesional" })
+                .eq("id_U", dataDecision.user_USP)
+
+            if (errorEstado) {
+                console.log("Error completo:", errorEstado)
+                throw new Error(errorEstado.message)
+            }
+        }
+
+        return res.status(200).json({ mensaje: "Solicitud actualizada con éxito" })
+    }
+    catch (err) {
+        if (estadoCambiado === true) {
+            const { error: errorRevertir } = await supabase
+                .from('SolicitudProfesional')
+                .update({ estado: "pendiente" })
+                .eq('id_SP', idSolicitud)
+                .single()
+        }
+
+        res.status(400).json({ mensaje: "Error al completar la actualizacion", error: err.message })
+    }
+
+
+
+}
+
+export { listarSolicitudesPendientes, actualizarSolicitud }
--- Backend/src/routes/adminRoute.js
+++ Backend/src/routes/adminRoute.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { listarSolicitudesPendientes } from "../controllers/adminController.js"
+import { actualizarSolicitud, listarSolicitudesPendientes } from "../controllers/adminController.js"
 import { verificarAdmin } from "../middlewares/verificarAdmin.js";
 import { verificacionToken } from "../middlewares/verificarToken.js";
 
 const router = Router()
 
 router.get('/solicitudes', verificacionToken, verificarAdmin, listarSolicitudesPendientes)
+
+router.patch('/solicitudes/:id', verificacionToken, verificarAdmin, actualizarSolicitud)
 
 export default router
