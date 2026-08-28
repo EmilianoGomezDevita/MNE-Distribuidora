d963a51310f0ce5f82eadb14fb330414aeaef5dc
EmilianoGomezDevita <gomezdevitae@gmail.com>
2026-08-28 08:19:41 -0300

    Controller de carrito para sumar o agregar productos al carrito

---
+++ Backend/src/controllers/caritoController.js
@@ -0,0 +1,78 @@
+import {supabase} from '../config/supabase.js'
+async function addAlCarrito(req, res) {
+    const idUsuario = req.usuario.id;
+    const {id_Producto, cant} = req.body;
+
+    try{
+         // Paso 1: buscar si el usuario ya tiene un carrito
+        const { data:carritoExiste, error: errorBuscarCarrito} = await supabase
+        .from('Carritos')
+        .select('id')
+        .eq('id_User', idUsuario)
+        .maybeSingle();// a diferencia de .single(), no tira error si no hay ninguna fila
+
+        if(errorBuscarCarrito){
+            throw new Error(errorBuscarCarrito.message)
+        }
+        let idCarrito;
+
+        if(carritoExiste){
+            idCarrito = carritoExiste.id
+        }else{
+            // Paso 2: no tenía carrito, se lo creamos
+            const {data: carritoNuevo, error: errorCrearCarrito} = await supabase
+            .from('Carritos')
+            .insert({ id_User: idUsuario })
+            .select('id')
+            .single();
+
+            if(errorCrearCarrito){
+                throw new Error(errorCrearCarrito.message)
+            }
+
+            idCarrito = carritoNuevo.id
+        }
+        // Paso 3: ¿ya tenía este producto en el carrito?
+        const { data: itemExistente, error: errorConsultaCant} = await supabase
+        .from('itemsCarrito')
+        .select('id_IC, cantidad')
+        .eq('id_C', idCarrito)
+        .eq('id_prod', id_Producto)
+        .maybeSingle()
+
+        if(errorConsultaCant){
+            throw new Error(errorConsultaCant.message)
+        }
+
+        // Paso 4: según el resultado del paso 3, insert o update sumando cantidad
+        if(itemExistente){
+            const nuevaCant = itemExistente.cantidad + cant;
+            const { error: errrorUpdate} = await supabase
+            .from('itemsCarrito')
+            .update({cantidad: nuevaCant})
+            .eq('id_IC', itemExistente.id_IC)
+            
+            if(errrorUpdate){
+                throw new Error(errrorUpdate.message)
+            }
+        }else{
+            const {error: errorInsert} = await supabase
+            .from('itemsCarrito')
+            .insert({
+                id_C: idCarrito,
+                id_prod: id_Producto,
+                cantidad: cant
+            })
+            if(errorInsert){
+                throw new Error(errorInsert.message)
+            }
+        }
+        res.status(201).json({mensaje: "Producto agregado al carrito"})
+    }
+    
+    catch(err){
+        return res.status(400).json({mensaje: "Error al agregar al carrito", error: err.message})
+    }
+}
+
+export { addAlCarrito };
