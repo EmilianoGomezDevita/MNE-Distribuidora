b684f8d652f122b9b099d59eba21530d0df9aad0
EmilianoGomezDevita <gomezdevitae@gmail.com>
2026-08-28 09:13:24 -0300

    ruta para obtener los items cargados en el carrito

--- Backend/src/routes/carritoRoute.js
+++ Backend/src/routes/carritoRoute.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { addAlCarrito } from "../controllers/caritoController.js";
+import { addAlCarrito } from "../controllers/carritoController.js";
 import { verificacionToken } from "../middlewares/verificarToken.js";
+import { ListarCarrito } from "../controllers/listarCarrito.js";
 
 const router = Router();
 
 router.post('/carrito', verificacionToken, addAlCarrito)
+
+router.get('/carrito', verificacionToken, ListarCarrito)
 
 export default router
