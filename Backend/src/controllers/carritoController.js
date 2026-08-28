cdbd260df87b2159ea3426e8d7c454d8785efb40
EmilianoGomezDevita <gomezdevitae@gmail.com>
2026-08-28 09:30:48 -0300

    funcion para eliminar item del carrito hecha

--- Backend/src/controllers/carritoController.js
+++ Backend/src/controllers/carritoController.js
@@ -1,77 +1,114 @@
-import {supabase} from '../config/supabase.js'
+import { supabase } from '../config/supabase.js'
+
 async function addAlCarrito(req, res) {
     const idUsuario = req.usuario.id;
-    const {id_Producto, cant} = req.body;
+    const { id_Producto, cant } = req.body;
 
-    try{
-         // Paso 1: buscar si el usuario ya tiene un carrito
-        const { data:carritoExiste, error: errorBuscarCarrito} = await supabase
-        .from('Carritos')
-        .select('id')
-        .eq('id_User', idUsuario)
-        .maybeSingle();// a diferencia de .single(), no tira error si no hay ninguna fila
+    try {
+        // Paso 1: buscar si el usuario ya tiene un carrito
+        const { data: carritoExiste, error: errorBuscarCarrito } = await supabase
+            .from('Carritos')
+            .select('id')
+            .eq('id_User', idUsuario)
+            .maybeSingle();// a diferencia de .single(), no tira error si no hay ninguna fila
 
-        if(errorBuscarCarrito){
+        if (errorBuscarCarrito) {
             throw new Error(errorBuscarCarrito.message)
         }
         let idCarrito;
 
-        if(carritoExiste){
+        if (carritoExiste) {
             idCarrito = carritoExiste.id
-        }else{
+        } else {
             // Paso 2: no tenía carrito, se lo creamos
-            const {data: carritoNuevo, error: errorCrearCarrito} = await supabase
-            .from('Carritos')
-            .insert({ id_User: idUsuario })
-            .select('id')
-            .single();
+            const { data: carritoNuevo, error: errorCrearCarrito } = await supabase
+                .from('Carritos')
+                .insert({ id_User: idUsuario })
+                .select('id')
+                .single();
 
-            if(errorCrearCarrito){
+            if (errorCrearCarrito) {
                 throw new Error(errorCrearCarrito.message)
             }
 
             idCarrito = carritoNuevo.id
         }
         // Paso 3: ¿ya tenía este producto en el carrito?
-        const { data: itemExistente, error: errorConsultaCant} = await supabase
-        .from('itemsCarrito')
-        .select('id_IC, cantidad')
-        .eq('id_C', idCarrito)
-        .eq('id_prod', id_Producto)
-        .maybeSingle()
+        const { data: itemExistente, error: errorConsultaCant } = await supabase
+            .from('itemsCarrito')
+            .select('id_IC, cantidad')
+            .eq('id_C', idCarrito)
+            .eq('id_prod', id_Producto)
+            .maybeSingle()
 
-        if(errorConsultaCant){
+        if (errorConsultaCant) {
             throw new Error(errorConsultaCant.message)
         }
 
         // Paso 4: según el resultado del paso 3, insert o update sumando cantidad
-        if(itemExistente){
+        if (itemExistente) {
             const nuevaCant = itemExistente.cantidad + cant;
-            const { error: errrorUpdate} = await supabase
-            .from('itemsCarrito')
-            .update({cantidad: nuevaCant})
-            .eq('id_IC', itemExistente.id_IC)
-            
-            if(errrorUpdate){
+            const { error: errrorUpdate } = await supabase
+                .from('itemsCarrito')
+                .update({ cantidad: nuevaCant })
+                .eq('id_IC', itemExistente.id_IC)
+
+            if (errrorUpdate) {
                 throw new Error(errrorUpdate.message)
             }
-        }else{
-            const {error: errorInsert} = await supabase
-            .from('itemsCarrito')
-            .insert({
-                id_C: idCarrito,
-                id_prod: id_Producto,
-                cantidad: cant
-            })
-            if(errorInsert){
+        } else {
+            const { error: errorInsert } = await supabase
+                .from('itemsCarrito')
+                .insert({
+                    id_C: idCarrito,
+                    id_prod: id_Producto,
+                    cantidad: cant
+                })
+            if (errorInsert) {
                 throw new Error(errorInsert.message)
             }
         }
-        res.status(201).json({mensaje: "Producto agregado al carrito"})
+        res.status(201).json({ mensaje: "Producto agregado al carrito" })
     }
 
+    catch (err) {
+        return res.status(400).json({ mensaje: "Error al agregar al carrito", error: err.message })
+    }
+}
+
+
+async function eliminarItem(req, res) {
+    const idItem = req.params.id;
+    const idUsuario = req.usuario.id;
+
+    try {
+        const { data: dataCarrito, error: errorBuscarCarrito } = await supabase
+            .from('Carritos')
+            .select('id')
+            .eq('id_User', idUsuario)
+            .maybeSingle();// a diferencia de .single(), no tira error si no hay ninguna fila
+
+        if (errorBuscarCarrito) {
+            throw new Error(errorBuscarCarrito.message)
+        }
+        if (!dataCarrito) {
+            return res.status(404).json({ mensaje: "No tenés ningún carrito" });
+        }
+
+        // Paso 2: borrar el item, pero SOLO si pertenece a este carrito
+        const { error: errorDelete} = await supabase
+        .from('itemsCarrito')
+        .delete()
+        .eq('id_IC', idItem)
+        .eq('id_C', dataCarrito.id)
+
+        if(errorDelete){
+            throw new Error(errorDelete.message)
+        }
+        return res.status(200).json({mensaje:"Producto eliminado del carrito"});
+    }
     catch(err){
-        return res.status(400).json({mensaje: "Error al agregar al carrito", error: err.message})
+        return res.status(400).json({mensaje:"Error al eliminar del carrito", error: err.message})
     }
 }
 
