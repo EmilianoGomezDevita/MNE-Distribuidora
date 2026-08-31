b45dc953371fff5ccf1f169d654876e581de1719
EmilianoGomezDevita <gomezdevitae@gmail.com>
2026-08-31 10:02:44 -0300

    feat: Implement getProdPorId function and update product routes

--- Backend/src/controllers/listarProductos.js
+++ Backend/src/controllers/listarProductos.js
@@ -1,11 +1,10 @@
 import { supabase } from "../config/supabase.js";
 import { manejarError } from "../utils/manejoErrores.js";
 
-
 async function ListarProductos(req, res) {
-    const { data: dataProds, error: errorProds } = await supabase
-        .from('Productos')
-        .select(`
+    const { data: dataProds, error: errorProds } = await supabase.from(
+        "Productos",
+    ).select(`
         id,
         nombre,
         descripcion,
@@ -22,19 +21,20 @@ async function ListarProductos(req, res) {
         return manejarError(res, 400, "Error al listar los productos", errorProds);
     }
 
-    let esProfesional = false
+    let esProfesional = false;
 
     if (req.usuario) {
         const { data: usuarioDB } = await supabase
-            .from('usuarios')
-            .select('tipoCuenta').eq('id_U', req.usuario.id).single()
+            .from("usuarios")
+            .select("tipoCuenta")
+            .eq("id_U", req.usuario.id)
+            .single();
 
         if (usuarioDB && usuarioDB.tipoCuenta === "Profesional") {
-            esProfesional = true
+            esProfesional = true;
         }
-
     }
-    const productosFinales = dataProds.map(prod => {
+    const productosFinales = dataProds.map((prod) => {
         return {
             id: prod.id,
             nombre: prod.nombre,
@@ -44,11 +44,75 @@ async function ListarProductos(req, res) {
             id_marca: prod.id_marca,
             id_categoria: prod.id_categoria,
             fechaVencimiento: prod.fechaVencimiento,
-            imagen: prod.ImagenesProducto.find(img => img.orden === 1)?.url
-        }
-    })
+            imagen: prod.ImagenesProducto.find((img) => img.orden === 1)?.url,
+        };
+    });
 
-    return res.status(200).json(productosFinales)
+    return res.status(200).json(productosFinales);
 }
 
-export { ListarProductos }
+async function getProdPorId(req, res) {
+    const idProd = req.params.id;
+
+    try {
+        //PASO 1: verificamos que el ID del producto exista en la DB y traemos la info del prod
+        const { data: dataProd, error: errorProd } = await supabase
+            .from("Productos")
+            .select(`id,
+                nombre,
+                descripcion,
+                precio,
+                precioProfesional,
+                stock,
+                id_marca,
+                id_categoria,
+                fechaVencimiento,
+                ImagenesProducto (url, orden)
+            `)
+            .eq("id", idProd)
+            .single();
+        //PASO 2: Validar si no se encontró
+        if (errorProd || !dataProd) {
+            return res.status(404).json({ mensaje: "Id de producto inexistente" });
+        }
+
+        //PASO 3:Verificar si es profesional
+        let esProfesional = false;
+
+        if (req.usuario) {
+            const { data: usuarioDB } = await supabase
+                .from("usuarios")
+                .select("tipoCuenta")
+                .eq("id_U", req.usuario.id)
+                .single();
+
+            if (usuarioDB && usuarioDB.tipoCuenta === "Profesional") {
+                esProfesional = true;
+            }
+        }
+
+        //PASO 4(opciones): traer productso relacionados por marca o categoria
+
+        //PASO 5: armado del map
+        const productoFinal = {
+            id: dataProd.id,
+            nombre: dataProd.nombre,
+            precio: esProfesional ? dataProd.precioProfesional : dataProd.precio,
+            descripcion: dataProd.descripcion,
+            stock: dataProd.stock,
+            id_marca: dataProd.id_marca,
+            id_categoria: dataProd.id_categoria,
+            fechaVencimiento: dataProd.fechaVencimiento,
+            // Mantenemos el array de imágenes para el carrusel del frontend
+            imagenes: dataProd.ImagenesProducto
+                ? dataProd.ImagenesProducto.sort((a, b) => a.orden - b.orden).map(img => img.url)
+                : [],
+        }
+        return res.status(200).json(productoFinal);
+    }
+    catch (err) {
+        manejarError(res, 500, "Error interno del servidor", err)
+    }
+}
+
+export { ListarProductos, getProdPorId };
--- Backend/src/routes/prodRoute.js
+++ Backend/src/routes/prodRoute.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { ListarProductos } from "../controllers/listarProductos.js";
+import { ListarProductos, getProdPorId } from "../controllers/listarProductos.js";
 import { identificarUsuario } from "../middlewares/identificarUsuario.js";
 
 const router = Router();
 
 router.get('/catalogo', identificarUsuario, ListarProductos)
+
+router.get('/catalogo/producto/:id', identificarUsuario, getProdPorId)
 
 export default router;
