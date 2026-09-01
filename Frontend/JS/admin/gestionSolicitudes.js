b974093a2feec012c79676e3e814b576b46bbe1d
EmilianoGomezDevita <gomezdevitae@gmail.com>
2026-09-01 08:56:17 -0300

    feat: Add management page for professional requests with dynamic card rendering

--- Frontend/JS/admin/gestionSolicitudes.js
+++ Frontend/JS/admin/gestionSolicitudes.js
@@ -1,4 +1,4 @@
-import { API_URL } from './config.js';
+import { API_URL } from '../config.js';
 
 document.addEventListener("DOMContentLoaded", function () {
     const token = localStorage.getItem('token');
