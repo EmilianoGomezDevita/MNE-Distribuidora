504714e6bfde67b6cec4a4b400d5e4927566edde
EmilianoGomezDevita <gomezdevitae@gmail.com>
2026-09-03 14:21:01 -0300

    feat: Implement product addition functionality with image upload

--- Backend/src/routes/adminRoute.js
+++ Backend/src/routes/adminRoute.js
@@ -1,12 +1,18 @@
 import { Router } from "express";
-import { actualizarSolicitud, listarSolicitudesPendientes } from "../controllers/adminController.js"
+import { ListarProductos } from "../controllers/listarProductos.js"
+import { actualizarSolicitud, listarSolicitudesPendientes, agregarProducto } from "../controllers/adminController.js"
 import { verificarAdmin } from "../middlewares/verificarAdmin.js";
 import { verificacionToken } from "../middlewares/verificarToken.js";
+import { upload } from "../middlewares/upload.js";
 
 const router = Router()
 
 router.get('/solicitudes', verificacionToken, verificarAdmin, listarSolicitudesPendientes)
 
 router.patch('/solicitudes/:id', verificacionToken, verificarAdmin, actualizarSolicitud)
+
+router.get('/productos',verificacionToken, verificarAdmin, ListarProductos)
+
+router.post('/producto/add',verificacionToken, verificarAdmin, upload.array('imagenes', 5), agregarProducto) 
 
 export default router
